4cf69cba679f29aa6774c045a26bdba914bcd05c
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-10-20 13:29:31 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,22 @@
+public class Max{
+public static void main(String[]js){
+byte districtsInKarnataka=30;
+System.out.println(districtsInKarnataka);
+short heightOfMountEverest=8848;
+System.out.println(heightOfMountEverest);
+int numberOfBirthsperday=140000000;
+System.out.println(numberOfBirthsperday);
+long ageOfMoon=45300000;
+System.out.println(ageOfMoon);
+float pHvalueOfAcidRAin=4.2f;
+System.out.println(pHvalueOfAcidRAin);
+double stdAtmosphere=1.0132502;
+System.out.println(stdAtmosphere);
+char gender='F';
+System.out.println(gender);
+boolean carFlyInAir=false;
+System.out.println(carFlyInAir);
+String hi="Update the system";
+System.out.println(hi);
+}
+}

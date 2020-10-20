5d773ed14cfa7a1adedadf2bdccb4ceb8d172ebd
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-10-20 13:30:31 +0530

    Add files via upload

---
+++ Tanu.java
@@ -0,0 +1,22 @@
+public class Tanu{
+public static void main(String[]js){
+byte zoosInIndia=18;
+System.out.println(zoosInIndia);
+short damsInIndia=5202;
+System.out.println(damsInIndia);
+int areaOfIndianOcean=73426000;
+System.out.println(areaOfIndianOcean);
+long rbcsInHumanBody=25000000;
+System.out.println(rbcsInHumanBody);
+float refractiveIndexOfGlass=1.5f;
+System.out.println(refractiveIndexOfGlass);
+double distanceBetweenEarthtoMars=65.580000000;
+System.out.println(distanceBetweenEarthtoMars);
+char thirdletterInAlphabets='C';
+System.out.println(thirdletterInAlphabets);
+boolean skyIsBlue=true;
+System.out.println(skyIsBlue);
+String Sam="Door is Open";
+System.out.println(Sam);
+}
+}

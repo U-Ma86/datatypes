b8d72b5cccb0689a498ca34df6810c0d2b7f7fb7
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-10-20 12:37:29 +0530

    my first commit

---
+++ Numbers.java
@@ -0,0 +1,24 @@
+public class Numbers{
+public static void main(String[]opt){
+byte alphabets =26;
+System.out.println(alphabets);
+short bonesOfHumanBody=206;
+System.out.println(bonesOfHumanBody); 
+int coronaCasesInWorld=40100000;
+System.out.println(coronaCasesInWorld);
+float weightInDigiScale=53.2250f;
+System.out.println(weightInDigiScale);
+long neuronsInHumanBrain=860000000;
+System.out.println(neuronsInHumanBrain);
+double valueOfPermitivity=8.854187817;
+System.out.println(valueOfPermitivity);
+char grade='A';
+System.out.println(grade);
+boolean milkIsBlack=false;
+System.out.println(milkIsBlack);
+String js= "Java is platform Independent";
+System.out.println(js);
+
+}
+
+}

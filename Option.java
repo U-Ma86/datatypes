a3950ddee694de68bf4a687ced4f77491c627bb8
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-10-20 12:38:26 +0530

    Add files via upload

---
+++ Option.java
@@ -0,0 +1,22 @@
+public class Option{
+public static void main(String[]choice){
+byte teeth=32;
+System.out.println(teeth);
+short secondsInOneHour=3600;
+System.out.println(secondsInOneHour);
+int spbSongs=40000;
+System.out.println(spbSongs);
+long distanceBetweenEarthAndSun=14899000;
+System.out.println(distanceBetweenEarthAndSun);
+float pHValueOfWater=8.5f;
+System.out.println(pHValueOfWater);
+double valueOfBoltzmanConstant=1.380649;
+System.out.println(valueOfBoltzmanConstant);
+char firstletterInFlag='F';
+System.out.println(firstletterInFlag);
+boolean treesAreGreen=true;
+System.out.println(treesAreGreen);
+String win="X-workz starts at 8:00AM";
+System.out.println(win);
+}
+}

3894b657d96b0f2d69fbac02218497408bc3e6f2
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-10-20 21:14:18 +0530

    Add files via upload

---
+++ Work.java
@@ -0,0 +1,27 @@
+class Myworld{
+public static void main(String[] Js){
+System.out.println("Home");
+family();
+}
+public static void family(){
+	System.out.println("Parents");
+	friends();
+}
+public static void friends(){
+	System.out.println("blood relations");
+	pet();
+}
+public static void pet(){
+	System.out.println("my life");
+	relatives();
+}
+	public static void relatives(){
+		System.out.println("kick out");
+	}
+}
+
+
+
+
+
+	

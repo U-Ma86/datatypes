13d77be1968c6b15318475b6a68e993f38101f3b
U-Ma86 <73157737+U-Ma86@users.noreply.github.com>
2020-11-04 18:12:54 +0530

    Add files via upload

---
+++ NEWprg/Biodata.java
@@ -0,0 +1,10 @@
+public class Biodata{
+public static void main(String[]info){
+final long adharNumber=582369740;
+System.out.println("adharNumber:"+adharNumber);
+final long phoneNumber=785265485;
+System.out.println("phoneNumber:"+phoneNumber);
+final String emailid="mahalakshmi@gmail.com";
+System.out.println("emailid:"+emailid);
+}
+}
